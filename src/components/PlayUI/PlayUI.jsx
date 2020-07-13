ce7ba1a7422ab6e36882702b5362515ba7cf7691
Padunk <abraham.agel@gmail.com>
2020-07-13 08:28:58 +0700

    add next track sign

--- src/components/PlayUI/PlayUI.jsx
+++ src/components/PlayUI/PlayUI.jsx
@@ -18,6 +18,8 @@ export const PlayUI = props => {
 
   const [playSong, setPlaySong] = React.useState(false)
   const [playingTrack, setPlayingTrack] = React.useState([])
+  const [isPlaying, setIsPlaying] = React.useState(false)
+  const [nextTrack, setNextTrack] = React.useState("")
 
   const handleClick = event => {
     const trackData = [
@@ -26,10 +28,20 @@ export const PlayUI = props => {
     ]
     setPlayingTrack(trackData)
     setPlaySong(true)
+    setIsPlaying(true)
+    setNextTrack(event.currentTarget.dataset.nextTrackName)
   }
+
+  const playOrPause = event => setIsPlaying(!isPlaying)
 
   return (
     <Container>
+      {playSong && nextTrack && (
+        <NextTrackTitle>
+          <span style={{ color: `${COLORS.white}` }}>NEXT:</span>
+          <span>{nextTrack}</span>
+        </NextTrackTitle>
+      )}
       <AlbumDetail>
         <ImgWrapper>
           <img
@@ -49,21 +61,29 @@ export const PlayUI = props => {
       {playSong && (
         <ControlWrapper>
           <TrackTitle>
-            <p>{playingTrack[0]}</p>
+            <PlayingSong>{playingTrack[0]}</PlayingSong>
+            {/* {nextTrack && (
+              <p style={{ color: `${COLORS.gray}` }}>NEXT: {nextTrack}</p>
+            )} */}
           </TrackTitle>
-          <TrackTimeLine>
+          <TrackTimeLine />
+          <TrackControlTime>
             <TrackTimeBegin>0.00</TrackTimeBegin>
             <TrackTimeLeft>
               -{convertSecondToMinute(playingTrack[1])}
             </TrackTimeLeft>
-          </TrackTimeLine>
+          </TrackControlTime>
           <Controller>
             <IconWrapper>
               <Icon icon={heart} size="24" />
             </IconWrapper>
             <IconWrapper className="play-pause">
-              <Icon icon={play} size="48" />
-              {/* <Icon icon={pause} size="40" /> */}
+              {!isPlaying && (
+                <Icon icon={play} size="48" onClick={playOrPause} />
+              )}
+              {isPlaying && (
+                <Icon icon={pause} size="48" onClick={playOrPause} />
+              )}
             </IconWrapper>
             <IconWrapper className="two-icons">
               <Icon icon={iosSkipbackward} size="32" />
@@ -81,15 +101,18 @@ export const PlayUI = props => {
       )}
 
       <TrackWrapper>
-        <p style={{ color: `${COLORS.grayLight}` }}>TRACKS</p>
+        <p style={{ color: `${COLORS.grayLight}` }}>SONGS</p>
         <Line />
         <ul>
-          {tracks.map(track => (
+          {tracks.map((track, idx) => (
             <TrackDetail
               key={track["@attr"].rank}
               onClick={handleClick}
               data-track-name={track.name}
               data-track-duration={track.duration}
+              data-next-track-name={
+                idx === tracks.length - 1 ? "" : tracks[idx + 1].name
+              }
               className={
                 playSong && playingTrack[0] === track.name
                   ? "active"
@@ -118,7 +141,7 @@ const TrackDetail = styled.li`
   list-style-type: none;
   display: flex;
   justify-content: space-between;
-  padding: 5px 0;
+  padding: 5px 1rem;
 
   &.active {
     background-color: ${COLORS.primary};
@@ -164,21 +187,31 @@ const IconWrapper = styled.div`
 const TrackTimeBegin = styled.span``
 const TrackTimeLeft = styled.span``
 
-const TrackTimeLine = styled.div`
+const TrackControlTime = styled.div`
   display: flex;
   justify-content: space-between;
-  position: relative;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
   font-size: 1.4rem;
   color: ${COLORS.grayLight};
+  padding-bottom: 0.5rem;
+`
+
+const TrackTimeLine = styled.span`
+  display: inline-block;
+  position: relative;
+  z-index: 1;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  width: 100%;
+  height: 2px;
+  background-color: ${COLORS.grayDark};
 
   ::before {
     content: "";
     position: absolute;
+    z-index: -1;
     top: 0;
     left: 0;
-    width: 100%;
+    width: 10%;
     height: 2px;
     background-color: ${COLORS.primary};
   }
@@ -187,12 +220,17 @@ const TrackTimeLine = styled.div`
     content: "";
     position: absolute;
     top: -4px;
-    left: 0;
+    left: 10%;
     width: 3px;
     height: 1rem;
     background-color: ${COLORS.grayLight};
     border-radius: 2px;
   }
+`
+
+const PlayingSong = styled.p`
+  font-size: 1.8rem;
+  padding-bottom: 5px;
 `
 
 const TrackTitle = styled.div`
@@ -236,13 +274,31 @@ const ImgWrapper = styled.div`
   border-radius: 1rem;
   display: inline-block;
 `
+
 const Container = styled.div`
   margin-left: 7rem;
   margin-top: 6rem;
   padding: 2rem;
-  background: black;
+  background: ${COLORS.black};
   color: ${COLORS.white};
   overflow-y: auto;
   display: grid;
   place-items: center;
+  position: relative;
+`
+
+const NextTrackTitle = styled.p`
+  position: fixed;
+  padding: 1rem 4rem 1rem 2rem;
+  top: 11rem;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid ${COLORS.grayLight};
+  border-bottom: 1px solid ${COLORS.grayLight};
+  border-left: 1px solid ${COLORS.grayLight};
+  color: ${COLORS.grayLight};
+  font-size: 1.6rem;
+  border-top-left-radius: 30px;
+  border-bottom-left-radius: 30px;
 `
